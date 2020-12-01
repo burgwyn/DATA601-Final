14ce1a83681137bdb5ebbf716c5c3861e333daee
Nat Burgwyn <burgwyn@gmail.com>
2020-11-30 23:49:50 -0500

    Add time util

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,3 +3,7 @@ from dateutil.parser import parse
 
 def format_date(date_str):
     return parse(date_str)
+
+
+def format_time(time_str):
+    return parse(time_str).time()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,3 +10,8 @@ class TestUtilsMethods(unittest.TestCase):
         expected = datetime.datetime(2018, 8, 25)
         actual = utils.format_date('8/25/2018, 12:00 AM')
         assert actual == expected
+
+    def test_format_time(self):
+        expected = datetime.time(19, 55)
+        actual = utils.format_time('07:55 PM')
+        assert actual == expected
