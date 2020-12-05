a07bca1b5dd0fc7dceec4bbe68777048d9b05356
Nat Burgwyn <burgwyn@gmail.com>
2020-12-04 21:03:13 -0500

    Make dirs

--- env.sh
+++ env.sh
@@ -2,6 +2,9 @@
 
 # create the data directory
 mkdir data
+mkdir data/sample
+mkdir data/incoming
+mkdir data/cleaned
 
-conda env create -f environment.yaml --name burgwyn_data601_final
-conda activate burgwyn_data601_final
+# conda env create -f environment.yaml --name burgwyn_data601_final
+# conda activate burgwyn_data601_final
