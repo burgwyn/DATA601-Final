72aaa460cbdd9a3b82b38d33eb175542318e1dfe
Nat Burgwyn <burgwyn_nathaniel@bah.com>
2020-12-04 23:50:41 -0500

    Update data paths

--- env.sh
+++ env.sh
@@ -5,6 +5,7 @@ mkdir data
 mkdir data/sample
 mkdir data/incoming
 mkdir data/cleaned
+mkdir data/config
 
 # conda env create -f environment.yaml --name burgwyn_data601_final
 # conda activate burgwyn_data601_final
--- scripts/main.py
+++ scripts/main.py
@@ -8,11 +8,11 @@ parking_data = do_search('Parking Violations Issued in')
 print(parking_data)
 
 # save response to data directory
-with open(os.path.join('data', 'parking_data.json'), 'w') as file:
+with open(os.path.join('data', 'config', 'parking_data.json'), 'w') as file:
     json.dump(parking_data, file)
 
 for key in parking_data.keys():
     url = parking_data[key]
     print(f'Fetching... {key}')
     file_name = key.replace(' ', '_')
-    urllib.request.urlretrieve(url, f'data/{file_name}.csv')
+    urllib.request.urlretrieve(url, f'data/incoming/{file_name}.csv')
