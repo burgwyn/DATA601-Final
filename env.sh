ae18c4e2c43209658be870a80131a57254f423d9
Nat Burgwyn <burgwyn_nathaniel@bah.com>
2020-12-08 00:45:22 -0500

    Add folder

--- env.sh
+++ env.sh
@@ -6,6 +6,7 @@ mkdir data/sample
 mkdir data/incoming
 mkdir data/cleaned
 mkdir data/config
+mkdir data/final
 
 conda env create -f environment.yaml --name burgwyn_data601_final
 conda activate burgwyn_data601_final
